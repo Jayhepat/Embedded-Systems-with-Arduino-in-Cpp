1331b38e7a76c37fd1776c45288c8dc00b3db153
Jay Hepat <162289805+Jayhepat@users.noreply.github.com>
2025-11-15 23:04:35 +0530

    Refactor buzzer tone generation and delays

--- Buzzer Interfacing/Buzzer.cpp
+++ Buzzer Interfacing/Buzzer.cpp
@@ -7,16 +7,26 @@ void setup() {
 }
 
 void loop() {
-    // Produce a 100 Hz tone
+    
     tone(BUZZERPin, 100);
-    delay(200); // Wait for 200 milliseconds
-    // Produce a 400 Hz tone
+    delay(200); 
+    
     tone(BUZZERPin, 400);
-    delay(200); // Wait for 200 milliseconds
-    // Produce a 600 Hz tone
+    delay(200); 
+    
     tone(BUZZERPin, 600);
-    delay(200); // Wait for 200 milliseconds
+    delay(200);
     // Stop the tone
     noTone(BUZZERPin);
-    delay(2000); // Wait for 2 seconds before repeating
+    delay(2000); 
 }
+
+
+// const int BUZZERPin = 9;: Specifies the pin number to which the buzzer is connected.
+// pinMode(BUZZERPin, OUTPUT);: Sets the buzzer pin as an output in the setup function.
+// tone(BUZZERPin, 100);: Generates a 100 Hz tone on the buzzer pin.
+// delay(200);: Delays the program for 200 milliseconds.
+// tone(BUZZERPin, 400);: Generates a 400 Hz tone.
+// tone(BUZZERPin, 600);: Generates a 600 Hz tone.
+// noTone(BUZZERPin);: Stops any tone being produced.
+// delay(2000);: Delays the program for 2 seconds before the loop repeats.
