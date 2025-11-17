b123b976c941a8da67f43d0c31865de7ef1b273e
Jay Hepat <162289805+Jayhepat@users.noreply.github.com>
2025-11-17 23:24:26 +0530

    Add MultiSwitchs.cpp for LED and switch control
    This code initializes two LEDs and two switches, reading the state of each switch to control the corresponding LED.

---
+++ Switchs and  LED Interfacing/MultiSwitchs.cpp
@@ -0,0 +1,37 @@
+const int LEDPin1= 12;
+const int LEDPin2 = 13;
+const int SwitchPin1 = 2;
+const int SwitchPin2 = 3;
+
+
+//variables to store switch inputs
+int SwitchState1 = 0;
+int SwitchState2 = 0;
+
+void setup(){
+  pinMode(LEDPin1, OUTPUT);
+  pinMode(LEDPin2, OUTPUT);
+
+  pinMode(SwitchPin1, INPUT);
+  pinMode(SwitchPin2, INPUT);
+}
+
+void loop(){
+  SwitchState1 = digitalRead(SwitchPin1);
+
+  if(SwitchPin1 == HIGH){               //Read the state of Switch 1
+    digitalWrite(LEDPin1, HIGH);
+  } else {
+    digitalWrite(LEDPin1, LOW);
+  }
+
+  SwitchState2 = digitalRead(SwitchPin2);       
+                                      // Read the sytate of switch 2
+  if(SwitchPin2 == HIGH){
+    digitalWrite(LEDPin2, HIGH);
+  } else {
+    digitalWrite(LEDPin2, LOW);
+  }
+
+  delay(15);
+}
