b82b9bce732e40757191aba01d68771c1b6b7043
Jay Hepat <162289805+Jayhepat@users.noreply.github.com>
2025-11-17 23:27:52 +0530

    Correct switch state checks and improve comments
    Fixed logic errors in switch state checks and added comments for clarity.

--- Switchs and  LED Interfacing/MultiSwitchs.cpp
+++ Switchs and  LED Interfacing/MultiSwitchs.cpp
@@ -9,25 +9,27 @@ int SwitchState1 = 0;
 int SwitchState2 = 0;
 
 void setup(){
+  //Initialize LED oins
   pinMode(LEDPin1, OUTPUT);
   pinMode(LEDPin2, OUTPUT);
 
+  //Initialize Switch Pins
   pinMode(SwitchPin1, INPUT);
   pinMode(SwitchPin2, INPUT);
 }
 
 void loop(){
-  SwitchState1 = digitalRead(SwitchPin1);
+  SwitchState1 = digitalRead(SwitchPin1);    //Read the state of Switch 1
 
-  if(SwitchPin1 == HIGH){               //Read the state of Switch 1
+  if(SwitchState1 == HIGH){               
     digitalWrite(LEDPin1, HIGH);
   } else {
     digitalWrite(LEDPin1, LOW);
   }
 
-  SwitchState2 = digitalRead(SwitchPin2);       
-                                      // Read the sytate of switch 2
-  if(SwitchPin2 == HIGH){
+  SwitchState2 = digitalRead(SwitchPin2);       // Read the sytate of switch 2
+  
+  if(SwitchState2 == HIGH){
     digitalWrite(LEDPin2, HIGH);
   } else {
     digitalWrite(LEDPin2, LOW);
