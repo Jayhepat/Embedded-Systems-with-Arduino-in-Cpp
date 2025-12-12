58da4340108a783a7e0a037bd0841dfda599a477
Jay Hepat <162289805+Jayhepat@users.noreply.github.com>
2025-12-12 12:51:48 +0530

    Implement LED control with button interrupt

---
+++ Interrupts/Interrupts_Logic.cpp
@@ -0,0 +1,25 @@
+volatile boolean ledOn = false;
+
+void setup(){
+  pinMode(13, OUTPUT);
+  pinMode(2, INPUT);
+  //attachInterrupt(0, ButtonPressed, RISING);
+  //Code Portable maps the pin to interrupt vector number
+  attachInterrupt(digitalPinToInterrupt(2) ,buttonPressed, RISING);
+}
+
+void loop(){
+
+}
+//ISR
+void buttonPressed()
+{
+  if(ledOn)
+  {
+    ledOn = false;
+    digitalWrite(13, LOW);
+  } else {
+    ledOn = true;
+    digitalWrite(13, HIGH);
+  }
+}
